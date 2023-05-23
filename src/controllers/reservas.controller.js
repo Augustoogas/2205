e2a47185d45681f300ec8453c7f195084a9adf31
Augusto ogas <augustoogas7@gmail.com>
2023-05-22 21:22:12 -0300

    termine la configuracion de reservas

--- src/controllers/reservas.controller.js
+++ src/controllers/reservas.controller.js
@@ -1,4 +1,5 @@
 const reservas= require('../datos/reservas.json');
+const vehiculos= require('../datos/vehiculos.json');
 
 //Traemos todas las reservas 
 const getAll=(req,res)=>{
@@ -19,20 +20,68 @@ const getReservasById=(req,res)=>{
     }
 }
 
+
+//encontramos el indice y lo borramos con splice()
 const deleteReservasById=(req,res)=>{
     const id=req.params.id;
 
     const indice=reservas.findIndex(r => r.id == id);
 
     if(indice>=0){
-        reservas.split(indice,1);
-
-        res.status(200).json({mensaje:`La reserva con el id ${id} fue borrado con exito`}
-        ,reservas);
+        reservas.splice(indice,1);
+        res.status(200).json({mensaje:`La reserva con el id ${id} fue borrado con exito`});
     }else{
         res.status(404).json({mensaje:`El id ${id} no existe`});
     }
 
 }
 
-module.exports={getAll,getReservasById,deleteReservasById};
+
+//Aqui creamos una reserva
+
+const postReservas=(req,res)=>{
+    const bodyDatos=req.body;
+    
+    const vehiculoHabilitado=vehiculos.find(v => v.habilitado &&
+        v.capacidad >= bodyDatos.cantPersonas && v.autonomiaKms>=bodyDatos.distancia);
+    
+
+    //Establecemos el mayor id
+    const ids=reservas.map( r => r.id);
+    mayorId=Math.max(...ids)+1;
+
+    //una expresion regular(regex) para validar la fecha
+    const re=/\d{8}/;
+    
+    
+    //validacion de datos y creacion de la reserva
+    if(((bodyDatos.cantPersonas >=1 && req.body.cantPersonas<=10) && bodyDatos.distancia < 500  
+    && re.test(bodyDatos.fecha)&& vehiculoHabilitado!=undefined)){
+        const reservaNueva={
+            "id":mayorId,
+            "cliente": bodyDatos.cliente,
+            "cantPersonas":bodyDatos.cantPersonas,
+            "distancia": bodyDatos.distancia,
+            "fecha":bodyDatos.fecha ,
+            "vehiculo":vehiculoHabilitado
+        }
+
+        
+        
+        
+
+        reservas.push(reservaNueva);
+        res.status(201).json(reservaNueva);
+
+
+    }else if(vehiculoHabilitado==undefined){
+        res.status(400).json({mensaje:`No un auto disponible para los requisitos que pide`});
+    }
+    
+    else{
+        //mensaje de error si alguno de los datos esta mal ingresado
+        res.status(400).json({mensaje:"Ingrese los datos correctamente"});
+    }
+}
+
+module.exports={getAll,getReservasById,deleteReservasById,postReservas};
--- src/routes/reserva.route.js
+++ src/routes/reserva.route.js
@@ -9,6 +9,9 @@ route.get("/:id",controllerReservas.getReservasById);
 route.delete("/:id",controllerReservas.deleteReservasById);
 
 
+route.post("/",controllerReservas.postReservas);
+
+
 
 
 module.exports=route;
